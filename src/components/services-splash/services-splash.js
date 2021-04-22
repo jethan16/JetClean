9f3ef81341bbbb9371258f5f195e80faeb94544e
Jethan16 <ethan.nel.jones@gmail.com>
2021-04-22 14:58:00 -0400

    added alternative DOM element for services splash

--- src/components/services-splash/services-splash.js
+++ src/components/services-splash/services-splash.js
@@ -8,6 +8,7 @@ import "../../utils/fontawesome"
 const servicesSplash = (props) => {
     return(
         <>
+        {/* MOBILE DOM */}
             <section className='services-splash'>
                 <div className='services-title'>
                     <h1>{props.cmsContent.servicesSplashTitle}</h1>
@@ -19,6 +20,19 @@ const servicesSplash = (props) => {
                     <ServicesToken text='Complete' icon={'check'} id={'token-3'}/>
                 </div>
                 <Button buttonStyle='alert-button' buttonText='See Our Services'/>
+            </section>
+            {/* DESKTOP DOM */}
+            <section className='services-splash-desktop'>
+                <div className='services-path-desktop'>
+                    <ServicesToken text='Restore' icon={'leaf'} id={'token-1'}/>
+                    <ServicesToken text='Protect' icon={'shield-alt'} id={'token-2'}/>
+                    <ServicesToken text='Complete' icon={'check'} id={'token-3'}/>
+                </div>
+                <div className='services-title-desktop'>
+                    <h1>{props.cmsContent.servicesSplashTitle}</h1>
+                    <h2>{props.cmsContent.servicesSplashSubtitle}</h2>
+                    <Button buttonStyle='alert-button' buttonText='See Our Services'/>
+                </div>
             </section>
         </>
     );
