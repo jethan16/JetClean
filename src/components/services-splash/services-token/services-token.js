65dc07f0d879044eb73f2e8df5dd67ea75205996
Jethan16 <ethan.nel.jones@gmail.com>
2021-04-22 15:05:12 -0400

    added responsiveness to services token

--- src/components/services-splash/services-token/services-token.js
+++ src/components/services-splash/services-token/services-token.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import {graphql} from 'gatsby';
 import "./services-token.css"
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
